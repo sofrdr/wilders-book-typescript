34331cfe85d79b97183f162aa911d54fa62ed1a3
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-03-15 17:26:39 +0100

    New form to create a wilder using react-hook-form

---
+++ client/src/components/AddWilderForm/AddWilderForm.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import "./AddWilderForm.css";
+
+const url = "http://localhost:5000/api/wilders";
+
+type Inputs = {
+  name: string;
+  city: string;
+  email: string;
+};
+
+const AddWilderForm = () => {
+  const [errorMsg, setErrorMsg] = useState<String>("");
+  const addWilder = async (wilder: Inputs) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(wilder),
+      });
+      const data = await response.json();
+      if (data.error) {
+        setErrorMsg(data.error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => addWilder(data);
+
+  return (
+    <section className="form-container">
+      <h2>Add a wilder</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="input-container">
+          <div className="form-input">
+            <label>Name</label>
+            <input {...register("name", { required: true })} />
+            {errors.name && <div className="error-msg">Name is required</div>}
+          </div>
+
+          <div className="form-input">
+            <label>Email</label>
+            <input type="email" {...register("email", { required: true })} />
+            {errors.email && <div className="error-msg">Email is required</div>}
+          </div>
+
+          <div className="form-input">
+            <label>City</label>
+            <input {...register("city")} />
+          </div>
+        </div>
+        {errorMsg && <div className="error-msg">{errorMsg}</div>}
+        <br />
+        <button type="submit" className="button">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default AddWilderForm;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import AddWilderForm from "../components/AddWilderForm/AddWilderForm";
 import WildersList from "../components/WildersList/WildersList";
 
 const Home = () => {
   return (
     <main className="container">
       <WildersList />
+      <AddWilderForm />
     </main>
   );
 };
