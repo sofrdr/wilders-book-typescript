c8d2cb2df5c0f041ca12e63ec0c05c69e6b2ef38
Sophie Roudier <sophie.roudier@hotmail.fr>
2023-03-16 14:31:33 +0100

    Function to remove a skill from a wilder

--- client/src/components/Skill/Skill.tsx
+++ client/src/components/Skill/Skill.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import ISkill from "../../utils/interfaces/ISkill";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import "./Skill.css";
 
 export type SkillProps = ISkill & {
   wilderId: number;
 };
 
-const Skill = ({ name }: SkillProps) => {
-  return <li>{name}</li>;
+const Skill = ({ name, id, wilderId }: SkillProps) => {
+  const [isOver, setIsOver] = useState<boolean>(false);
+
+  const removeSkill = async (id: number, skillId: number) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/wilders/${id}/skill/${skillId}/delete`,
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <li
+      onMouseEnter={() => setIsOver(true)}
+      onMouseLeave={() => setIsOver(false)}
+    >
+      {name}
+      <div
+        className={isOver ? "delete-btn" : "hide"}
+        onClick={() => removeSkill(wilderId, id)}
+      >
+        <AiOutlineCloseCircle />
+      </div>
+    </li>
+  );
 };
 
 export default Skill;
